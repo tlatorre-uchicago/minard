f4110e065634d8ecb88146b7c97af95b0b373eda
Anthony LaTorre <none@none>
2013-10-14 14:45:47 -0500

    update fluid layout

--- minard/views.py
+++ minard/views.py
@@ -4,3 +4,9 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('layout.html',message='Hello World!')
+
+links=[{'href':'/', 'name': 'test', 'group': 'Hello'}]
+
+@app.route('/fluid')
+def fluid():
+    return render_template('fluid.html',links=links)
