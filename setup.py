3e50002d124eb74cd74616733121749a98afb0b4
Anthony LaTorre <none@none>
2013-10-15 15:20:42 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Minard',
+      version='0.1',
+      description='Web App Monitoring Tools',
+      author='Anthony LaTorre',
+      author_email='tlatorre@uchicago.edu',
+      url='',
+      install_requires=['flask']
+      )
