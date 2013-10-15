5dcd33fd3dc3827e102302137ee63246c5ddddfe
Anthony LaTorre <none@none>
2013-10-15 00:23:44 -0500

    add sphere projection

--- minard/views.py
+++ minard/views.py
@@ -1,5 +1,5 @@
 from minard import app
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
 PROJECT_NAME = 'Minard'
 
@@ -8,20 +8,51 @@ def project_name():
     return {'project_name': PROJECT_NAME}
 
 import random
+import json
 
 @app.route('/get')
 def get():
+    name = request.args.get('name','',type=str)
+    print name
+    if name == 'sphere':
+        print 'returning id and values'
+        id = []
+        values = []
+        for i in range(9000):
+            id.append(i)#random.randint(0,9000))
+            values.append(1)
+        return jsonify(id=id,values2=values)
     return jsonify(values=[random.gauss(5,1) for i in range(100)])
 
 @app.route('/')
 def index():
     return render_template('layout.html',message='Hello World!')
 
+with open('/home/tlatorre/monitor/proj/pmt.js') as f:
+    pmtinfo = json.load(f)
+
+import math
+
+coords = []
+count = 0
+for x, y, z in zip(pmtinfo['x'],pmtinfo['y'],pmtinfo['z']):
+    r = math.sqrt(x**2 + y**2 + z**2)
+    if r <= 0:
+        count += 1
+        coords.append([0,0])
+        continue
+
+    t = math.acos(z/r)*180.0/math.pi - 90.0
+    p = math.atan2(y,x)*180.0/math.pi
+    coords.append([p,t]);
+
+print 'count = ', count
+
 sidebar=[{'href': '/', 'name': 'test', 'group': 'Hello'}]
 nav = [{'href': '/', 'name': 'test'}]
-containers = [{'name': 'Histogram %i' % i, 'type': 'histogram'} for i in range(2)]
+containers = [{'name': 'Histogram %i' % i, 'type': 'histogram'} for i in range(2)] + []
 
 
 @app.route('/fluid')
 def fluid():
-    return render_template('fluid.html',sidebar=sidebar,nav=nav,containers=containers)
+    return render_template('fluid.html',sidebar=sidebar,nav=nav,containers=containers,hero_unit={'name': 'test', 'type': 'sphere-projection', 'coords':json.dumps(coords)})
