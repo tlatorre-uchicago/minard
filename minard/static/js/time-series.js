1fc1b9cd74840db551dd2cca5e2c5e0647eca575
Anthony LaTorre <none@none>
2013-10-18 15:34:04 -0500

    update time-series.js

--- minard/static/js/time-series.js
+++ minard/static/js/time-series.js
@@ -1,8 +1,8 @@
 function timeSeries(url) {
 
-var margin = {top: 6, right: 0, bottom: 20, left: 40},
-    width = 960 - margin.right,
-    height = 120 - margin.top - margin.bottom;
+    var margin = {top: 6, right: 0, bottom: 20, left: 40},
+        width = 960 - margin.right,
+        height = 120 - margin.top - margin.bottom;
 
     var n = 243,
         duration = 750;
@@ -10,104 +10,105 @@ var margin = {top: 6, right: 0, bottom: 20, left: 40},
     function chart(selection) {
         selection.each(function(name) {
 
-                var now = new Date(Date.now() - duration);
-
-            $.getJSON(url, {'name': name,
-                            'first': now - (n - 2) * duration,
-                            'last': now - duration,
-                        'npoints': n}).done(plot).fail(function(_, __, err) {alert(err);});
+            var now = new Date(Date.now() - duration);
 
             var element = this;
 
+            $.getJSON(url, {'name': name + 'hello',
+                            'first': now - (n - 2) * duration,
+                            'last': now - duration,
+                            'npoints': n})
+                .done(plot)
+                .fail(function(_, __, err) {alert(err);});
+
             function plot(response) {
 
-                //alert(data);
-
-                data = response.data;
+                var data = response.data;
 
                 var x = d3.time.scale()
                         .domain([now - (n - 2) * duration, now - duration])
                         .range([0, width]);
 
-var y = d3.scale.linear()
-    .domain([-10,10])
-    .range([height, 0]);
+                var y = d3.scale.linear()
+                    .domain([d3.min(data),d3.max(data)])
+                    .range([height, 0]);
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d, i) { return x(now - (n - 1 - i) * duration); })
-    .y(function(d, i) { return y(d); });
+                var line = d3.svg.line()
+                    .interpolate("basis")
+                    .x(function(d, i) {
+                        return x(now - (n - 1 - i) * duration); })
+                    .y(function(d, i) { return y(d); });
 
-var svg = d3.select(element).append("p").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style("margin-left", -margin.left + "px")
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                var svg = d3.select(element).append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .style("margin-left", -margin.left + "px")
+                  .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+                svg.append("defs").append("clipPath")
+                    .attr("id", "clip")
+                  .append("rect")
+                    .attr("width", width)
+                    .attr("height", height);
 
-var axis = svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
+                var axis = svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
 
-var path = svg.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .data([data])
-    .attr("class", "line");
+                var path = svg.append("g")
+                    .attr("clip-path", "url(#clip)")
+                  .append("path")
+                    .data([data])
+                    .text(String(data))
+                    .attr("class", "line");
 
+                var update = function(){
+                    return $.getJSON(url,{'name': name + 'world',
+                                          'last': null});
+                }
 
-var update = function(){
-    return $.getJSON(url,{'name': name, 'last': null});
-}
+                setInterval(function(){
+                        update()
+                            .done(tick)
+                            .fail(function(_, __, err) { alert(err);});
+                },duration);
 
-setInterval(function(){
-        update().done(tick).fail(function(_, __, err) { alert(err);});
-},duration);
+                function tick(response) {
+                    var value = response.value;
 
-function tick(response) {
+                    // update the domains
+                    now = new Date();
+                    x.domain([now - (n - 2) * duration, now - duration]);
+                    y.domain([d3.min(data), d3.max(data)]);
 
-    var value = response.value;
+                    // push the accumulated count onto the back, and reset the count
+                    data.push(value);
 
-  // update the domains
-  now = new Date();
-  x.domain([now - (n - 2) * duration, now - duration]);
-  y.domain([0, d3.max(data)]);
+                      // redraw the line
+                      svg.selectAll(".line")
+                          .attr("d", line)
+                          .attr("transform", null);
 
-  // push the accumulated count onto the back, and reset the count
-    data.push(value);
+                      // slide the x-axis left
+                      axis.transition()
+                          .duration(duration)
+                          .ease("linear")
+                          .call(x.axis);
 
-  // redraw the line
-  svg.select(".line")
-      .attr("d", line)
-      .attr("transform", null);
+                      // slide the line left
+                      path.transition()
+                          .duration(duration)
+                          .ease("linear")
+                          .attr("transform", "translate(" + x(now - (n - 1) * duration) + ")")
+                            //.each("end", tick);
 
-  // slide the x-axis left
-  axis.transition()
-      .duration(duration)
-      .ease("linear")
-      .call(x.axis);
-
-  // slide the line left
-  path.transition()
-      .duration(duration)
-      .ease("linear")
-      .attr("transform", "translate(" + x(now - (n - 1) * duration) + ")")
-      .each("end", tick);
-
-  // pop the old data point off the front
-  data.shift();
-
-}
-
+                      // pop the old data point off the front
+                      data.shift();
+                }
             }
-            });
+        });
     }
     return chart;
 }
