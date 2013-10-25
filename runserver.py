6744ab723719d6d546fe54ae85119f15bce0d12f
Anthony LaTorre <none@none>
2013-10-24 20:12:29 -0500

    update setup

--- runserver.py
+++ runserver.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 from minard import app
+import gevent
+from gevent.wsgi import WSGIServer
+import gevent.monkey
+gevent.monkey.patch_all()
 
-app.run(host='0.0.0.0')#debug=True)
+server = WSGIServer(('',5000),app)
+server.serve_forever()
--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='Minard',
       author='Anthony LaTorre',
       author_email='tlatorre@uchicago.edu',
       url='',
-      install_requires=['flask']
+      install_requires=['flask','flask-login','gevent']
       )
