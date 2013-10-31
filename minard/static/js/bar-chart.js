f39a2ef95a1a58453f749dfe01060190306ee324
Anthony LaTorre <none@none>
2013-10-31 17:40:22 -0500

    update bar-chart.js

--- minard/static/js/bar-chart.js
+++ minard/static/js/bar-chart.js
@@ -1,36 +1,53 @@
 function bar_chart() {
-    var margin = {top: 6, right: 0, bottom: 20, left: 40},
-        width = 960 - margin.right,
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
         height = 120 - margin.top - margin.bottom;
+
+    var svg;
 
     function chart(selection) {
         selection.each(function(data) {
-        var x = d3.scale.ordinal().rangeRoundBands([0,width]);
-        var y = d3.scale.linear().range([height,0]);
+        var x = d3.scale.ordinal().rangeRoundBands([0,width],0.1)
+            .domain(data.x);
+        var y = d3.scale.linear().range([height,0])
+            .domain([0,d3.max(data.y)]);
 
         var x_axis = d3.svg.axis().scale(x).orient('bottom');
         var y_axis = d3.svg.axis().scale(y).orient('left');
 
-        var svg = d3.select(this).append('svg')
-            .attr('width', width)
-            .attr('height', height)
+        if (!svg) {
+            svg = d3.select(this).append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+              .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-        svg.append('g').attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(x_axis);
+            svg.append('g').attr('class', 'x axis')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(x_axis);
 
-        svg.append('g').attr('class', 'y axis').call(y_axis)
+            svg.append('g').attr('class', 'y axis').call(y_axis);
+        }
 
-        svg.selectAll('.bar')
-            .data(data.y)
-          .enter().append('rect')
+        var bars = svg.selectAll('.bar')
+            .data(data.y);
+
+        bars.enter().append('rect')
             .attr('class', 'bar')
-            .attr('x', function(d) { return x(d.x); })
+            .attr('x', function(d, i) { return x(data.x[i]); })
             .attr('width', x.rangeBand())
-            .attr('y', function(d) { return y(d.y); })
-            .attr('height', function(d) { return height - y(d.y); });
+            .attr('y', function(d) { return y(d); })
+            .attr('height', function(d) { return height - y(d); });
 
-        });}
+        bars.transition()
+            .attr('x', function(d, i) { return x(data.x[i]); })
+            .attr('width', x.rangeBand())
+            .attr('y', function(d) { return y(d); })
+            .attr('height', function(d) { return height - y(d); });
+
+        bars.exit().transition().remove();
+
+       });}
 
     return chart;
 }
