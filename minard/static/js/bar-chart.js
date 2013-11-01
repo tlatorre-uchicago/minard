5bb6dcfdd2f17de45943d4cb1daa7725551f1356
Anthony LaTorre <none@none>
2013-11-01 14:57:51 -0500

    update bar chart and add example

--- minard/static/js/bar-chart.js
+++ minard/static/js/bar-chart.js
@@ -5,12 +5,21 @@ function bar_chart() {
 
     var svg;
 
+    var click = function(d, i) { return; };
+    var click_bg = function() { return; };
+
     function chart(selection) {
         selection.each(function(data) {
+        var data_x = [],
+            data_y = [];
+        for (var key in data) {
+            data_x.push(key);
+            data_y.push(data[key]);
+        }
         var x = d3.scale.ordinal().rangeRoundBands([0,width],0.1)
-            .domain(data.x);
+            .domain(data_x);
         var y = d3.scale.linear().range([height,0])
-            .domain([0,d3.max(data.y)]);
+            .domain([0,d3.max(data_y)]);
 
         var x_axis = d3.svg.axis().scale(x).orient('bottom');
         var y_axis = d3.svg.axis().scale(y).orient('left');
@@ -27,30 +36,48 @@ function bar_chart() {
                 .call(x_axis);
 
             svg.append('g').attr('class', 'y axis').call(y_axis);
+
+            svg.append('rect')
+                .attr('fill', 'white')
+                .attr('width', width)
+                .attr('height', height)
+                .on('click', click_bg);
         }
 
         svg.select('.x.axis').transition().call(x_axis);
         svg.select('.y.axis').transition().call(y_axis);
 
         var bars = svg.selectAll('.bar')
-            .data(data.y);
+            .data(data_x);
 
         bars.enter().append('rect')
             .attr('class', 'bar')
-            .attr('x', function(d, i) { return x(data.x[i]); })
+            .attr('x', function(d, i) { return x(data_x[i]); })
             .attr('width', x.rangeBand())
-            .attr('y', function(d) { return y(d); })
-            .attr('height', function(d) { return height - y(d); });
+            .attr('y', function(d, i) { return y(data_y[i]); })
+	    .attr('height', function(d, i) { return height - y(data_y[i]); })
+	    .on('click', click);
 
         bars.transition()
-            .attr('x', function(d, i) { return x(data.x[i]); })
+            .attr('x', function(d, i) { return x(data_x[i]); })
             .attr('width', x.rangeBand())
-            .attr('y', function(d) { return y(d); })
-            .attr('height', function(d) { return height - y(d); });
+            .attr('y', function(d, i) { return y(data_y[i]); })
+            .attr('height', function(d, i) { return height - y(data_y[i]); });
 
         bars.exit().transition().style({opacity: 0}).remove();
 
        });}
 
+       chart.click = function(value) {
+           if (!arguments.length) return click;
+           click = value;
+           return chart;
+       }
+
+       chart.click_bg = function(value) {
+           if (!arguments.length) return click_bg;
+           click_bg = value;
+           return chart;
+       }
     return chart;
 }
