d0aa75d00a409cb337988be9e1e97a71e45a00a3
Anthony LaTorre <none@none>
2013-11-14 16:12:29 -0600

    add Events table

--- minard/database.py
+++ minard/database.py
@@ -1,5 +1,9 @@
 import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dbinfo import user, passwd, host, name
+
+Base = declarative_base()
 
 engine = sa.create_engine('mysql://%s:%s@%s/%s' % (user,passwd,host,name))
 engine.connect()
@@ -9,4 +13,9 @@ meta.reflect(bind=engine)
 
 tables = meta.tables
 
-print tables
+class Events(Base):
+    __table__ = sa.Table('Events', meta, autoload=True)
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
