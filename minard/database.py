950e01eb36dcbcbffd20af730c406436e3b54800
Anthony LaTorre <none@none>
2013-12-18 20:13:47 -0600

    pool_recycle 3600->60 to keep mysql connection alive

--- minard/database.py
+++ minard/database.py
@@ -5,7 +5,7 @@ from dbinfo import user, passwd, host, name
 
 Base = declarative_base()
 
-engine = sa.create_engine('mysql://%s:%s@%s/%s' % (user,passwd,host,name), pool_recycle=3600)
+engine = sa.create_engine('mysql://%s:%s@%s/%s' % (user,passwd,host,name), pool_recycle=60)
 engine.connect()
 
 meta = sa.MetaData()
