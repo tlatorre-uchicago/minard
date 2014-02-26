7964491bb18ca76a2e0dbfa202ae429eebc5196a
Anthony LaTorre <none@none>
2014-02-26 11:27:00 -0600

    update setup and add init scripts
    --HG--
    rename : orca_server.py => scripts/orca_server
    rename : redis.conf => utils/redis.conf

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from glob import glob
 
 setup(name='Minard',
       version='0.1',
       description='Web App Monitoring Tools',
       author='Anthony LaTorre',
       author_email='tlatorre@uchicago.edu',
-      url='',
-      install_requires=['flask','flask-login','gunicorn','sqlalchemy','numpy','pyzmq','redis']
+      url='snopl.us',
+      scripts=['scripts/workers'],
+      data_files = [# init scripts
+                    ('/etc/init.d', ['utils/gunicorn','utils/snoplusmon']),
+                    # static files
+                    ('/var/www/minard/static/css',   [glob('minard/static/css/*')]),
+                    ('/var/www/minard/static/js',    [glob('minard/static/js/*')]),
+                    ('/var/www/minard/static/fonts', [glob('minard/static/fonts/*')])],
+      install_requires=['flask','gunicorn','sqlalchemy','numpy','pyzmq','redis']
       )
