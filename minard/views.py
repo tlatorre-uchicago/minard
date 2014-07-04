d4f5b03910b635e44531790c7ea7f0becc2103eb
Anthony LaTorre <none@none>
2014-07-04 17:18:09 -0500

    update list of programs. fix bug in status.html. add status to navbar.

--- minard/views.py
+++ minard/views.py
@@ -17,7 +17,10 @@ Program = namedtuple('Program', ['name', 'machine', 'link'])
 
 redis = Redis()
 
-PROGRAMS = [Program('L2',None,None)]
+PROGRAMS = [Program('builder','builder1.sp.snolab.ca',None),
+            Program('L2','buffer1.sp.snolab.ca',None),
+            Program('dataflow',None,
+                'http://snoplus.westgrid.ca:5984/buffer/_design/buffer/index.html')]
 
 @app.route('/status')
 def status():
