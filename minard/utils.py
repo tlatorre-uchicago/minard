2fd9963092fc032e3ec75b38fc5063ff5f1f5f71
Anthony LaTorre <none@none>
2014-07-08 11:37:25 -0500

    delete utils.py

--- minard/utils.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call, PIPE, Popen
-import os
-
-def pkill(name):
-    p = Popen(['pgrep','-f',name],stdout=PIPE)
-    for pid in map(int,p.communicate()[0].split()):
-        if pid != os.getpid():
-            print 'killing process %i' % pid
-            call(['kill',str(pid)])
