1661bcf8de47701b39267127896dcf7d80489a80
Freija Descamps <fbdescamps@lbl.gov>
2014-08-10 06:32:14 -0700

    Removed spurious links and views

--- minard/views.py
+++ minard/views.py
@@ -379,55 +379,5 @@ def pca_run_detail(run_number):
     return render_template('pca_run_detail.html',
                             run_number=run_number)      
 
-@app.route('/hello_world')
-def hello_world():
-    return render_template('hello_world.html')
-    
-@app.route('/hello_world_hist')    
-def hello_world_hist():
-    return jsonify(value=[random.gauss(0,1) for i in range(100)])
-    
-@app.route('/system_monitor')
-def system_monitor():
-    if not request.args.get('step'):
-        return redirect(url_for('system_monitor',step=1,height=20,_external=True))
-    step = request.args.get('step',1,type=int)
-    height = request.args.get('height',40,type=int)
-    return render_template('system_monitor.html',step=step,height=height)
-
-@app.route('/hello_world_metric')
-def hello_world_metric():
-    args = request.args
-
-    expr = args.get('expr',type=str)
-    start = args.get('start',type=parseiso)
-    stop = args.get('stop',type=parseiso)
-    now_client = args.get('now',type=parseiso)
-    # convert ms -> sec
-    step = args.get('step',type=int)//1000
-
-    now = int(time.time())
-
-    # adjust for clock skew
-    dt = now_client - now
-    start -= dt
-    stop -= dt
-
-    if step > 60:
-        t = 60
-    else:
-        t = 1
-
-    p = redis.pipeline()
-    for i in range(start,stop,step):
-        p.get('stream/%i:%i:%s' % (t,i//t,expr))
-    result = [float(x) if x else 0 for x in p.execute()]
-
-    if t == 60:
-        p = redis.pipeline()
-        for i in range(start,stop,step):
-            p.get('stream/60:%i:count' % (i//t))
-        counts = [int(x) if x else 0 for x in p.execute()]
-        result = [a/b for a, b in zip(result,counts)]
-
-    return jsonify(values=result)    
+  
+     
