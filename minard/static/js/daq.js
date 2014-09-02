d69c949a391088edf8340e393cba0f010bac25d9
Anthony LaTorre <tlatorre@uchicago.edu>
2014-09-02 09:47:48 -0500

    remove old daq stuff

--- minard/static/js/daq.js
+++ minard/static/js/daq.js
@@ -45,30 +45,6 @@ var crate = crate_view()
         $('#carousel').carousel('next');
     });
 
-if (threshold > 1000) {
-    card = card.format(d3.format('.2s'));
-}
-
-$('#threshold').val(crate.threshold());
-
-// Set threshold when <Enter> key is pressed
-$('#threshold').keyup(function(event) {
-  if (event.keyCode == 13) {
-    crate.threshold($('#threshold').val());
-    card.threshold($('#threshold').val());
-    update();
-  }
-});
-                    
-function warn(jqxhr, text_status, error) {
-    var err = text_status + ', ' + error;
-    $('#status').text(err).attr('class', 'alert alert-warning');
-}
-
-function success() {
-    $('#status').attr('class', 'alert alert-success').text('Connected');
-}
-
 var interval = 5000;
 
 function update() {
@@ -77,8 +53,7 @@ function update() {
         .done(function(result) {
             d3.select('#crate').datum(result.values).call(crate);
             d3.select('#card').datum(result.values).call(card);
-            success()
-        }).fail(warn);
+        });
 }
 
 d3.select('#crate').datum([]).call(crate);
