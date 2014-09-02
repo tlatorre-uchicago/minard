72cd90ae3a9d9a342b156d021f7554a9252238d4
Anthony LaTorre <tlatorre@uchicago.edu>
2014-09-01 21:31:15 -0500

    add function to get timeseries for hashes

--- minard/timeseries.py
+++ minard/timeseries.py
@@ -26,7 +26,51 @@ def get_hash_interval(step):
     return HASH_INTERVALS[bisect.bisect_right(HASH_INTERVALS,step)-1]
 
 def get_interval(step):
-    return INTERVALS[bisect.bisect_right(INTERVALS,step)-1]
+return INTERVALS[bisect.bisect_right(INTERVALS,step)-1]
+
+def get_hash_timeseries(name, start, stop, step, crate, card=None,
+                        channel=None, method='avg', type=None):
+    """
+    Returns the time series for `name` from `start` to `stop` in
+    increments of `step`.
+
+    `start` and `stop` should be UNIX timestamps. `step` should have
+    units of seconds.
+
+    If `card` is None, return results for the entire crate by
+    computing `method`, which may be `max` or `avg` for the maximum
+    and average value respectively.
+
+    If `channel` is None, return results for the entire card.
+    """
+    interval = get_hash_interval(step)
+
+    p = redis.pipeline()
+    for i in range(start, stop, step):
+        key = 'ts:%i:%i:%s' % (interval, i//interval, name)
+        if card is None:
+            # crate
+            if method == 'max':
+                cratemax(key, crate, client=p)
+            else:
+                crateavg(key, crate, client=p)
+        elif channel is None:
+            # card
+            if method == 'max':
+                cardmax(key, crate, card, client=p)
+            else:
+                cardavg(key, crate, card, client=p)
+        else:
+            # channel
+            i = crate*16*32 + card*32 + channel
+            p.hget(key, i)
+
+        values = p.execute()
+
+        if type is None:
+            return values
+
+        return map(type, values)
 
 def get_timeseries(name, start, stop, step, type=None):
     """
