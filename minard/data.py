77e2a641d50a5bf2c299a82891d084428dc3e22a
Anthony LaTorre <tlatorre@uchicago.edu>
2016-04-18 18:25:46 -0400

    read cmos counts and base currents from the data server

--- minard/data.py
+++ minard/data.py
@@ -10,42 +10,32 @@ from redis import Redis
 import time
 from minard.redistools import hmincrby, hmincrbyfloat, hdivh, hmincr, setavgmax
 from minard.timeseries import HASH_INTERVALS, HASH_EXPIRE
-from minard.tools import parseiso
-
-CMOS_ID = 1310720
-BASE_ID = 1048576
+from snotdaq import DataStream
 
 redis = Redis()
 
 # number of seconds to keep CMOS records
 EXPIRE = 60
 
-def strpiso(string):
-    """Converts an iso string -> python datetime object."""
-    try:
-        return datetime.strptime(string,'%Y-%m-%dT%H:%M:%S.%f')
-    except ValueError:
-        return datetime.strptime(string,'%Y-%m-%dT%H:%M:%S')
-
 def parse_cmos(rec):
     """Parse a CMOS record."""
-    crate, slot_mask = struct.unpack('II', rec[:8])
-    channel_mask = np.frombuffer(rec[8:8+4*16], dtype=np.uint32)
-    delay, error_flags = struct.unpack('II',rec[72:72+2*4])
-    counts = np.frombuffer(rec[80:80+8*32*4], dtype=np.uint32)
-    date_string = rec[21*4+8*32*4-4:].strip('\x00')
-    timestamp = parseiso(date_string)
+    crate, slot_mask = struct.unpack('>II', rec[:8])
+    channel_mask = np.frombuffer(rec[8:8+4*16], dtype='>u4')
+    error_flags = struct.unpack('>I',rec[72:72+4])
+    counts = np.frombuffer(rec[76:76+8*32*4], dtype='>u4')
+    # XL3 should really send us the timestamp, but for now
+    # we just use the time we receive it
+    timestamp = time.time()
     return crate, slot_mask, channel_mask, delay, error_flags, counts, timestamp
 
 def parse_base(rec):
     """Parse a base current record."""
-    crate, slot_mask = struct.unpack('II', rec[:8])
-    channel_mask = np.frombuffer(rec[8:8+4*16], dtype=np.uint32)
-    error_flags = struct.unpack('I',rec[72:72+4])
-    counts = np.frombuffer(rec[76:76+16*32], dtype=np.uint8).reshape((16,-1))
-    busy = np.frombuffer(rec[76+16*32:76+16*32+16*32], dtype=np.uint8).reshape((16,-1))
-    date_string = rec[76+2*16*32:].strip('\x00')
-    timestamp = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+    crate, slot_mask = struct.unpack('>II', rec[:8])
+    channel_mask = np.frombuffer(rec[8:8+4*16], dtype='>u4')
+    error_flags = struct.unpack('>I',rec[72:72+4])
+    counts = np.frombuffer(rec[76:76+16*32*4], dtype='>u4').reshape((16,-1))
+    busy = np.frombuffer(rec[76+16*32*4:76+16*32*4+16*32*4], dtype='>u4').reshape((16,-1))
+    timestamp = time.time()
     return crate, slot_mask, channel_mask, error_flags, counts, busy, timestamp
 
 def unpack_index(index):
@@ -108,7 +98,7 @@ def cmos_consumer(port):
             time.sleep(0.1)
             continue
 
-        if id != CMOS_ID:
+        if id != 'CMOS':
             raise ValueError('Expected CMOS record, got record %i' % id)
 
         crate, slotmask, channelmask, delay, error_flags, counts, timestamp = \
@@ -167,7 +157,7 @@ def base_consumer(port):
             time.sleep(0.1)
             continue
 
-        if id != BASE_ID:
+        if id != 'BASE':
             raise ValueError("Expected base current record got id %i" % id)
 
         crate, slotmask, channelmask, error_flags, counts, busy, timestamp = \
@@ -182,109 +172,15 @@ def base_consumer(port):
 
                 base_currents[index] = value-127
 
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') -> ABC DEF Gxx
-    args = [iter(iterable)]*n
-    return izip_longest(fillvalue=fillvalue, *args)
-
-def parse_header(header):
-    """Parse an ORCA header -> python dictionary."""
-    root = XML(header)
-    assert root.tag == 'plist'
-
-    def parse_item(item):
-        if item.tag == 'integer':
-            return int(item.text)
-        elif item.tag == 'string':
-            return item.text
-        elif item.tag == 'dict':
-            d = {}
-            for key, value in grouper(item,2):
-                d[key.text] = parse_item(value)
-            return d
-        elif item.tag == 'array':
-            return [parse_item(x) for x in item]
-        elif item.tag == 'false':
-            return False
-        elif item.tag == 'true':
-            return True
-        elif item.tag == 'real':
-            return float(item.text)
-        else:
-            raise Exception("can't parse %s" % item.tag)
-
-    return [parse_item(x) for x in root]
-
-class Socket(object):
-    """
-    Socket object used to communicate with ORCA.
-    See `here <http://orca.physics.unc.edu/~markhowe/Data_Format_Viewing/Data_Format.html>`_
-    for more information.
-    """
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-    def connect(self, host, port):
-        self.sock.connect((host,port))
-
-    def is_short(self, datarecord):
-        return (datarecord & 0x80000000) == 0x80000000
-
-    def get_dataid(self, datarecord):
-        """Returns the data id associated with `datarecord`."""
-        if self.is_short(datarecord):
-            return datarecord & 0xfc000000
-        else:
-            return datarecord & 0xfffc0000
-
-    def get_length(self, datarecord):
-        if self.is_short(datarecord):
-            return 1
-        else:
-            return datarecord & 0x3ffff
-
-    def send(self, msg):
-        totalsent = 0
-        while totalsent < len(msg):
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError('socket connection broken')
-
-            totalsent += sent
-
-    def recv(self, size):
-        msg = ''
-        while len(msg) < size:
-            chunk = self.sock.recv(size-len(msg))
-            if chunk == '':
-                raise RuntimeError('socket connection broken')
-            msg += chunk
-        return msg
-
-    def recv_record(self):
-        rec, = struct.unpack('I',self.recv(4))
-
-        if self.is_short(rec):
-            return self.get_dataid(rec), rec & 0x3ffffff
-        else:
-            # -1 because data record counts
-            size = self.get_length(rec)*4 - 4
-
-            return self.get_dataid(rec), self.recv(size)
-
-def orca_producer(host, port=44666):
+def data_producer(host, port=4000):
     """
     Pushes CMOS and base current records to a ZMQ Push/Pull socket
     to be parsed by other workers. CMOS rates and base currents are
     pushed to ports 5557 and 5558 respectively.
     See `zeromq.org <http://zeromq.org>`_ for more information.
     """
-    socket = Socket()
-    socket.connect(host, port)
+    data = DataStream(host, port=port, subscriptions=['CMOS','BASE'])
+    data.connect()
 
     cmos_context = zmq.Context()
     cmos = cmos_context.socket(zmq.PUSH)
@@ -296,8 +192,8 @@ def orca_producer(host, port=44666):
 
     while True:
         id, rec = socket.recv_record()
-        if id == CMOS_ID:
+        if id == 'CMOS':
             cmos.send_pyobj((id,rec))
 
-        if id == BASE_ID:
+        if id == 'BASE':
             base.send_pyobj((id,rec))
