ca04b027bdefd91c36c8d6ab2e7c977a73c7ae65
Anthony LaTorre <tlatorre@uchicago.edu>
2016-09-30 16:50:27 -0500

    update README and requirements.txt

--- minard/__init__.py
+++ minard/__init__.py
@@ -35,9 +35,7 @@ class ReverseProxied(object):
 
 app = Flask(__name__)
 
-app.debug = True
-
-app.config.from_envvar('MINARD_SETTINGS', silent=True)
+app.config.from_envvar('MINARD_SETTINGS', silent=False)
 
 app.wsgi_app = ReverseProxied(app.wsgi_app)
 
