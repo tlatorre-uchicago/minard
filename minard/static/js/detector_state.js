110005fe2046f836a840568af423585cf72f5043
Eric Marzec <marzece@sas.upenn.edu>
2016-10-20 20:35:54 -0400

    Revert "Make enabled triggers in threshold adapt to color changes better"
    This reverts commit 1aeeaac0421e14ee892183169f264cf28e99676a.

--- minard/static/js/detector_state.js
+++ minard/static/js/detector_state.js
@@ -515,7 +515,7 @@ function display_mtca_thresholds(node,dacs,trigger_scan,enabled_dacs){
             { return ""; }
             if(enabled_dacs && enabled_dacs[key])
             { return 'on'; }
-            return '';
+            return 'off';
         })
         .selectAll('td')
         .data( function(key,i) {
@@ -651,7 +651,9 @@ function crate() {
     var width = 780;
     var height = 80;
 
-    function my(){ }
+    function my(){
+
+    }
     my.width = function(value) {
         if(!arguments.length) {
             return width;
@@ -669,11 +671,9 @@ function crate() {
     return my;
 }
 function change_colors(class_name,color) {
-    crate_elements = d3.selectAll("td."+class_name)
-    text_elements = d3.selectAll("tr."+class_name)
-    other_elements = d3.selectAll("."+class_name+":not(tr):not(td)")
-    crate_elements.attr("style","background-color:"+color)
-    other_elements.attr("style","fill:"+color);
-    text_elements = d3.selectAll("tr."+class_name);
-    text_elements.attr("style","color:"+color);
+    var cols = document.getElementsByClassName(class_name);
+    for(i=0;i<cols.length;i++) {
+        cols[i].style.fill = color;
+        cols[i].style['background-color'] = color;
+    }
 }
