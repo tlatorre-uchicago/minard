8e30ef2c9ecf5a0a7fef939ffbdf55ed0e6d6730
Anthony LaTorre <tlatorre@uchicago.edu>
2016-11-08 18:40:21 -0500

    undo close connections since we've been seeing database errors on the detector state page recently

--- minard/detector_state.py
+++ minard/detector_state.py
@@ -18,8 +18,6 @@ def get_latest_trigger_scans():
 
     result = conn.execute("select distinct on (name) * from trigger_scan order by name, key desc")
 
-    conn.close()
-
     if result is None:
         return None
 
@@ -37,7 +35,6 @@ def fetch_from_table_with_key(table_name, key, key_name='key'):
     command = "SELECT * FROM %s WHERE %s = %s" % (table_name, key_name, key)
     res =  conn.execute(command)
 
-    conn.close()
     try:
         values = zip(res.keys(),res.fetchone())
     except TypeError:
