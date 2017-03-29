7a6ebd63729f38bcfa4d7c417f813323fb8b945d
Anthony LaTorre <tlatorre@uchicago.edu>
2017-03-29 11:22:59 -0400

    update

--- minard/channeldb.py
+++ minard/channeldb.py
@@ -15,8 +15,10 @@ class ChannelStatusForm(Form):
     cable_pulled =       BooleanField('Cable pulled')
     bad_cable =          BooleanField('Bad Cable')
     resistor_pulled =    BooleanField('Resistor pulled')
-    other =              BooleanField('Other')
+    disable_n100 =       BooleanField('Disable N100')
+    disable_n20 =        BooleanField('Disable N20')
     bad_base_current =   BooleanField('Bad Base Current')
+    name =               StringField('Name', [validators.Length(min=1)])
     info =               StringField('Info', [validators.Length(min=1)])
 
 def get_channels(crate=None, slot=None, channel=None, limit=100):
@@ -104,5 +106,5 @@ def get_channel_status_form(crate, slot, channel):
 
 def upload_channel_status(form):
     conn = engine.connect()
-    result = conn.execute("INSERT INTO channeldb (crate, slot, channel, low_occupancy, zero_occupancy, screamer, bad_discriminator, no_n100, no_n20, no_esum, cable_pulled, bad_cable, resistor_pulled, other, bad_base_current, info) VALUES (%(crate)s, %(slot)s, %(channel)s, %(low_occupancy)s, %(zero_occupancy)s, %(screamer)s, %(bad_discriminator)s, %(no_n100)s, %(no_n20)s, %(no_esum)s, %(cable_pulled)s, %(bad_cable)s, %(resistor_pulled)s, %(other)s, %(bad_base_current)s, %(info)s)", **form.data)
+    result = conn.execute("INSERT INTO channeldb (crate, slot, channel, low_occupancy, zero_occupancy, screamer, bad_discriminator, no_n100, no_n20, no_esum, cable_pulled, bad_cable, resistor_pulled, disable_n100, disable_n20, bad_base_current, name, info) VALUES (%(crate)s, %(slot)s, %(channel)s, %(low_occupancy)s, %(zero_occupancy)s, %(screamer)s, %(bad_discriminator)s, %(no_n100)s, %(no_n20)s, %(no_esum)s, %(cable_pulled)s, %(bad_cable)s, %(resistor_pulled)s, %(disable_n100)s, %(disable_n20)s, %(bad_base_current)s, %(name)s, %(info)s)", **form.data)
     return result
