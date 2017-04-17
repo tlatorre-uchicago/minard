5143879bac6bc7708cfc156db84ccc331ae4d7d8
Anthony LaTorre <tlatorre@uchicago.edu>
2017-04-17 15:06:55 -0400

    add high dropout column to channel status and channel status update pages

--- minard/channeldb.py
+++ minard/channeldb.py
@@ -25,6 +25,7 @@ class ChannelStatusForm(Form):
     resistor_pulled =    BooleanField('Resistor pulled')
     disable_n100 =       BooleanField('Disable N100')
     disable_n20 =        BooleanField('Disable N20')
+    high_dropout =       BooleanField('High Dropout')
     bad_base_current =   BooleanField('Bad Base Current')
     name =               StringField('Name', [validators.Length(min=1)])
     reason =             StringField('Reason')
@@ -257,10 +258,10 @@ def upload_channel_status(form):
         "(crate, slot, channel, pmt_removed, pmt_reinstalled, low_occupancy, "
         "zero_occupancy, screamer, bad_discriminator, no_n100, no_n20, "
         "no_esum, cable_pulled, bad_cable, resistor_pulled, disable_n100, "
-        "disable_n20, bad_base_current, name, reason, info) "
+        "disable_n20, high_dropout, bad_base_current, name, reason, info) "
         "VALUES (%(crate)s, %(slot)s, %(channel)s, %(pmt_removed)s, "
         "%(pmt_reinstalled)s, %(low_occupancy)s, %(zero_occupancy)s, "
         "%(screamer)s, %(bad_discriminator)s, %(no_n100)s, %(no_n20)s, "
         "%(no_esum)s, %(cable_pulled)s, %(bad_cable)s, %(resistor_pulled)s, "
-        "%(disable_n100)s, %(disable_n20)s, %(bad_base_current)s, %(name)s, "
-        "%(reason)s, %(info)s)", form.data)
+        "%(disable_n100)s, %(disable_n20)s, %(high_dropout)s, "
+        "%(bad_base_current)s, %(name)s, %(reason)s, %(info)s)", form.data)
