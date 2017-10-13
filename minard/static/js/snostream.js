086e283c09c74b91d9b57e062161f2630a0bc55c
Anthony LaTorre <tlatorre@uchicago.edu>
2017-10-13 10:50:32 -0400

    add the n16 source to the stream page

--- minard/static/js/snostream.js
+++ minard/static/js/snostream.js
@@ -98,6 +98,7 @@ function add_baseline_horizon(expressions, format, colors, extent, baseline, mv_
 add_horizon(TRIGGER_NAMES,format_rate);
 if (url_params.display == 'fecd') {
     add_horizon(TRIGGER_NAMES.slice(1,6).map(function(x) { return "FECD/" + x }),format_rate);
+    add_horizon(["FECD/N16"],format_rate);
 }
 add_horizon(["0\u03bd\u03b2\u03b2"],format_rate);
 add_horizon(["TOTAL-nhit","TOTAL-charge","PULGT-nhit","PULGT-charge","EXTA-nhit"], format('.2s'));
